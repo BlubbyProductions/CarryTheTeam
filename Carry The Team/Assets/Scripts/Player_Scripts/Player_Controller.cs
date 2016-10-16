9fea77e7eb66aa78f910cc070ac383c463f10daa
FanMan <aquasoulx@yahoo.com>
2016-10-16 18:03:31 -0400

    added a test stage

---
+++ Carry The Team/Assets/Scripts/Player_Scripts/Player_Controller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Controller : MonoBehaviour {
+
+	GameObject player;
+	public Rigidbody2D prb;
+	// Use this for initialization
+	void Start () {
+		prb = GetComponent<Rigidbody2D>();
+		player = gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space")) {
+			prb.AddForce (new Vector2(0, 3), ForceMode2D.Impulse);
+
+		}
+	}
+}
