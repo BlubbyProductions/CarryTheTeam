0262d7d06e725eeaef677ceca598537140aa6d7c
Animalwithnofur <Tripwwe@yahoo.com>
2016-10-19 22:42:10 -0400

    Added Death pit, Death Balls

---
+++ Carry The Team/Assets/Scripts/Objects/DeathBall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/*
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+			Destroy (gameObject);
+	}
+	*/
+
+	void OnTriggerEnter(Collider other)
+	{
+			Destroy(other.gameObject);
+	}
+
+
+}
