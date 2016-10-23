7bd741622480d9f0fa0c1214c1bd1307b4db30e0
Rafael Dalcolmo <lyganesh@live.com>
2016-10-23 19:14:39 -0400

    Networking: Added basic networking to the client.
    Added basic networking capabilities to initiate the prototype
    multiplayer.

--- Carry The Team/Assets/Scripts/Player_Scripts/Player_Controller.cs
+++ Carry The Team/Assets/Scripts/Player_Scripts/Player_Controller.cs
@@ -1,34 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class Player_Controller : MonoBehaviour {
-
-	public float MaxMoveSpeed;
-	public float MaxJumpHeight = 30.0f;
+using UnityEngine.Networking;
 
 
+public class Player_Controller : NetworkBehaviour
+{
+    public float MaxMoveSpeed;
+    public float MaxJumpHeight = 30.0f;
 
-	float dT; // deltaTime
+    float dT; // deltaTime
 
-	Rigidbody rbplayer;
+    Rigidbody rbplayer;
 
-	// Use this for initialization
-	void Start () {
-		rbplayer = GetComponent<Rigidbody> ();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rbplayer = GetComponent<Rigidbody>();
 
-	void Update()  {
-		
-	}
+    }
 
-	// fixed update is used when dealing with rigidbody calculations
-	void FixedUpdate () {
-		float horizontal = Input.GetAxis ("Horizontal");
-		float jump = Input.GetAxis ("Jump");
-		Vector3 move = new Vector3 (horizontal, jump, 0.0f);
+    void Update()
+    {
 
-		rbplayer.AddForce (move * MaxMoveSpeed);
+    }
 
-		Debug.Log (move * MaxMoveSpeed);
-	}
+    // fixed update is used when dealing with rigidbody calculations
+    void FixedUpdate()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float jump = Input.GetAxis("Jump");
+        Vector3 move = new Vector3(horizontal, jump, 0.0f);
+
+        rbplayer.AddForce(move * MaxMoveSpeed);
+
+        Debug.Log(move * MaxMoveSpeed);
+    }
 }
