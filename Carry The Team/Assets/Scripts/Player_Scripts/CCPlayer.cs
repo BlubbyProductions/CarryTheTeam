fcb837f8e5c926c83b55916a803a34cac6dea30a
FanMan <aquasoulx@yahoo.com>
2016-10-23 17:16:50 -0400

    Fixing up Character movement

---
+++ Carry The Team/Assets/Scripts/Player_Scripts/CCPlayer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CCPlayer : MonoBehaviour {
+
+	public float speed;
+	public float jumpHeight;
+	public float fallSpeed;
+	private bool isGrounded;
+	private float gravity;
+
+	CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+		gravity = -9.8f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		IsGrounded ();
+		IsFalling ();
+		Movement ();
+		Debug.Log (isGrounded);
+	}
+
+	void Movement() {
+		float horizontal = Input.GetAxis ("Horizontal");
+		//Vector3 move = new Vector3(horizontal, 0.0f, 0.0f);
+		//controller.Move (new Vector3(horizontal, 0.0f, 0.0f));
+	}
+
+	void IsFalling() {
+		if (!isGrounded) {
+			fallSpeed += gravity * Time.deltaTime;
+		}
+
+		controller.Move (new Vector3(0.0f, fallSpeed * Time.deltaTime, 0.0f));
+	}
+
+	void IsGrounded() {
+		//isGrounded = Physics.Raycast (transform.position, -transform.up, controller.height / 1.9f);
+		isGrounded = controller.isGrounded;
+	}
+}
--- Carry The Team/Assets/Scripts/Player_Scripts/Player_Controller.cs
+++ Carry The Team/Assets/Scripts/Player_Scripts/Player_Controller.cs
@@ -3,37 +3,32 @@ using System.Collections;
 
 public class Player_Controller : MonoBehaviour {
 
-	Transform myTransform;
-	public float verticalSpeed = 10.0f;
-	public float jumpHeight = 15.0f;
+	public float MaxMoveSpeed;
+	public float MaxJumpHeight = 30.0f;
+
+
+
 	float dT; // deltaTime
 
 	Rigidbody rbplayer;
-	public GameObject player;
 
 	// Use this for initialization
 	void Start () {
-		player = gameObject;
-		myTransform = transform;
-		//rbplayer = player.AddComponent<Rigidbody2D>() as Rigidbody2D;
+		rbplayer = GetComponent<Rigidbody> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		dT = Time.deltaTime;
 
-		if (Input.anyKey) {
-			switch (Input.inputString) {
-			case "a": // character moves left
-				player.GetComponent<Rigidbody>().velocity = new Vector3 (-verticalSpeed * dT, 0.0f, 0.0f);
-				break;
-			case "d": // character moves right
-				player.GetComponent<Rigidbody>().velocity = new Vector3 (verticalSpeed * dT, 0.0f, 0.0f);
-				break;
-			/*case "space":
-				player.GetComponent<Rigidbody2D> ().AddForce = new Vector2 (0.0f, jumpHeight * dT);
-				break;*/
-			}
-		}
+	void Update()  {
+		
+	}
+
+	// fixed update is used when dealing with rigidbody calculations
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		float jump = Input.GetAxis ("Jump");
+		Vector3 move = new Vector3 (horizontal, jump, 0.0f);
+
+		rbplayer.AddForce (move * MaxMoveSpeed);
+
+		Debug.Log (move * MaxMoveSpeed);
 	}
 }
