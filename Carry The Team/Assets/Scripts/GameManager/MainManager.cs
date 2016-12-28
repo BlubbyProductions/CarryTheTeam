40a3f623cdf838b65f8bb72f8161b70f741499f8
FanMan <aquasoulx@yahoo.com>
2016-12-27 22:55:35 -0500

    Added a clock timer for the minigames and finished Greenlight Go

--- Carry The Team/Assets/Scripts/GameManager/MainManager.cs
+++ Carry The Team/Assets/Scripts/GameManager/MainManager.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class MainManager : MonoBehaviour
 {
+    // 
 
+    // the lives of each player
+    private int P1Lives, P2Lives, P3Lives, P4Lives;
 
 	void Start ()
 	{
-		
+        P1Lives = 4;
+        P2Lives = 4;
+        P3Lives = 4;
+        P4Lives = 4;
+
+
 	}
 	
 	void Update ()
