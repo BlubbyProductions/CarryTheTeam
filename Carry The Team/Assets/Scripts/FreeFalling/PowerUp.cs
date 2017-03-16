d7d7ab28218e7ffce5b3a63c51c17f0a0bfe05da
FanMan <aquasoulx@yahoo.com>
2017-03-16 00:22:12 -0400

    Added a new minigame called "Free Falling" where players try to race while falling through the air. First (or last one standing) to reach the bottom wins.

---
+++ Carry The Team/Assets/Scripts/FreeFalling/PowerUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    // variables to hold power ups
+    // slow other players
+    // go faster
+    // go slower
+
+    // randomize which power up spawns
+
+    public float TimeBetweenSpawns;
+    
+
+    void Update()
+    {
+
+    }
+
+    void SpawnPowerUp()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider Player)
+    {
+        if(Player.CompareTag("Player"))
+        {
+            print("Player 1 got the power up");
+        }
+    }	
+}
